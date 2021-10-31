e3b4224891b0abe3f0efbb399216f33eca99ab75
abadfish <kirsten.ofarrell@gmail.com>
2021-10-31 08:44:50 -0400

    added routes for repos index page and repo show page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Repos from './pages/Repos'
+import Repo from './pages/Repos/Repo'
 
 function App() {
   return (
-    <div >
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path='/' component={ Repos } />
+        <Route path='/repos/:id' component={ Repo } />
+      </Switch>
+    </BrowserRouter>
+
   )
 }
 
--- src/pages/Repos/index.tsx
+++ src/pages/Repos/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Search from '../../components/Search'
 
 const Repos = () => {
   return (
     <div>
-      
+      <Search />
+      <h1>search results</h1>
     </div>
   )
 }

a5108e739a66fa6d340f9cb2a07544d421437fa5
abadfish <kirsten.ofarrell@gmail.com>
2021-10-31 21:50:17 -0400

    added reducer to Provider and added types for state, actions

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,17 +1,55 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 const initialState = {
   loading: false,
   repos: []
 }
 
+enum ActionType {
+  SUCCESSFUL_REPOS_FETCH = "SUCCESSFUL_REPOS_FETCH",
+}
+
+interface State {
+  repos?: any,
+  loading: boolean,
+  fetchRepos: ((term: string) => Promise<any>) | undefined
+}
+
+interface Action {
+  type: ActionType,
+  payload: any
+}
+
+
+const reducer: React.Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case ActionType.SUCCESSFUL_REPOS_FETCH:
+      return { 
+        ...state,
+        loading: false,
+        repos: action.payload 
+      }
+      default:
+        return state
+    }
+  }
+
 const ReposContext = createContext(initialState)
 
-const ReposProvider = () => {
+const ReposProvider: React.FC = ({ children }) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const values = {
+    loading: state.loading
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <ReposContext.Provider value={ values }>
+      { children }
+    </ReposContext.Provider>
+
   )
 }
 

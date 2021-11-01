3cc7edda44076006ccbb4918f05931b46fbbafa8
abadfish <kirsten.ofarrell@gmail.com>
2021-11-01 07:08:23 -0400

    added link to details page from repos index

--- src/pages/Repos/Repo.tsx
+++ src/pages/Repos/Repo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Repo = () => {
   return (
     <div>
-      
+      repo details page
     </div>
   )
 }
--- src/pages/Repos/index.tsx
+++ src/pages/Repos/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import Search from '../../components/Search'
 import { ReposContext } from '../../Provider'
 
@@ -8,7 +9,7 @@ const Repos: React.FC = () => {
   console.log(repos)
   
   const repoList = repos?.map((r:any) => (
-    <div>{ r.name }</div>
+    <div><Link to={`/repos/{ r.id }`}>{ r.name }</Link></div>
   ))
 
   loading && <div>Loading...</div>

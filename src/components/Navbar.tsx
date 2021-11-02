8ba6274cefde57d1e9a817f75cbf913143a752b6
abadfish <kirsten.ofarrell@gmail.com>
2021-11-02 10:55:51 -0400

    semantic navbar with screen adjusting font size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Navbar = () => {
+const Navbar:React.FC = () => {
   return (
-    <div>
-      <h1>NAVBAR</h1>
-    </div>
+    <Nav>
+      <h1>Github Repo Search Tool</h1>
+    </Nav>
   )
 }
 
 export default Navbar
 
+const Nav = styled.nav `
+  width: 100%;
+  height: 4vh;
+  background-color: lightblue;
+  padding: 1rem 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h1 {
+    margin: 0;
+    font-size: calc(14px + 6 * ((100vw - 100px) / 680));
+  }
+  @media (max-width: 768px) {
+    text-align: center;
+    padding: 1rem 0;
+  }
+`
